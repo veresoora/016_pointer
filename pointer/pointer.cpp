cacfac2659f0a0dd915d158268699f8361d3b964
veresoora <lahanabira@gmail.com>
2023-05-10 15:04:39 +0700

    mengimplementasikan method diluar class

--- pointer/pointer.cpp
+++ pointer/pointer.cpp
@@ -4,5 +4,9 @@ using namespace std;
 class mahasiswa {
 public:
 	int nim;
-	void showNim(); 
+	void showNim(); //deklarasi method
 };
+
+void mahasiswa::showNim() {
+	cout << "No Induk = " << nim << endl;
+}
